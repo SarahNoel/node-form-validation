8cda4ccf3817ef2af194b3fa6a17303bb90eb004
Sarah Turek <snturek@gmail.com>
2015-08-23 20:11:59 -0600

    Can add people and puppies, shows errors

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+var errArray =[];
 var puppyArray = [];
+var peopleArray = [];
+
+var nameErr = 'Name cannot be blank';
+var hobbyErr = 'Hobby cannot be blank';
+var idErr = "ID cannot be blank";
+var idNumErr = "An ID must be at least 3 characters long";
 
 router.get('/', function(req, res, next) {
   res.redirect('/puppy/new');
@@ -9,19 +16,65 @@ router.get('/', function(req, res, next) {
 
 router.get('/puppy/new', function(req, res, next) {
   res.render('puppies', {
-    title: 'Add a Puppy'});
+    title: 'Add a Puppy', errArray:errArray});
+});
+
+router.post('/addPup', function(req, res) {
+  res.redirect('/puppy/new');
 });
 
 router.post('/submit', function(req, res, next) {
   var name = req.body.name;
   var id = req.body.id;
+  errArray = [];
+  if(id === ""){
+    errArray.push(idErr);
+  }if(name === ""){
+    errArray.push(nameErr);
+  }if(id.length < 3){
+    errArray.push(idNumErr);
+  }if(errArray.length > 0){
+  res.redirect('/puppy/new');
+  }else{
   puppyArray.push({name: name, id: id});
-  res.redirect('/puppies');
+  errArray = [];
+  res.redirect('/puppies');}
   });
 
 router.get('/puppies', function(req, res, next) {
-  res.render('showPuppies', {title:'Showing Puppies'});
+  res.render('showPuppies', {title:'Showing Puppies', puppyArray:puppyArray});
+});
 
+
+
+///adding people ///
+router.get('/people/new', function(req, res, next) {
+  res.render('people', {
+    title: 'Add a Person', errArray:errArray});
+});
+
+router.post('/submit2', function(req, res, next) {
+  var name = req.body.name;
+  var hobby = req.body.hobby;
+  errArray = [];
+  if(name === ""){
+    errArray.push(nameErr);
+  }if(hobby === ""){
+    errArray.push(hobbyErr);
+  }if(errArray.length>0){
+    res.redirect('/people/new');
+  }else{
+  peopleArray.push({name: name, hobby: hobby});
+  res.redirect('/people');}
+  });
+
+router.get('/people', function(req, res, next) {
+  res.render('showPeople', {title:'Showing People', peopleArray:peopleArray});
+
+});
+
+router.post('/addPeep', function(req, res) {
+  res.redirect('/people/new');
 });
 
 
